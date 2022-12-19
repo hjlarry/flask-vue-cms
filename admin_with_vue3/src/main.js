76de012693a4cb9e3eb00e942558f162b9993e4c
hejl <hjlarry@163.com>
2022-12-19 14:42:44 +0800

    add login page view

--- admin_with_vue3/src/main.js
+++ admin_with_vue3/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import installElementPlus from './plugins/element'
+import './styles/index.scss'
 
 const app = createApp(App)
 installElementPlus(app)
--- admin_with_vue3/src/views/login/index.vue
+++ admin_with_vue3/src/views/login/index.vue
@@ -1,9 +1,162 @@
 <template>
-  <div class="login">
-    <h1>Login</h1>
+  <div class="login-container">
+    <el-form
+      autoComplete="on"
+      :model="loginForm"
+      :rules="loginRules"
+      ref="loginForm"
+      label-position="left"
+      label-width="0px"
+      class="card-box login-form"
+    >
+      <div class="title-container"><h3 class="title">flask-vue-cms</h3></div>
+
+      <el-form-item prop="username">
+        <span class="svg-container svg-container_login">
+          <svg-icon icon-class="user" />
+        </span>
+        <el-input
+          name="username"
+          type="text"
+          v-model="loginForm.username"
+          autoComplete="on"
+          placeholder="username"
+        />
+      </el-form-item>
+      <el-form-item prop="password">
+        <span class="svg-container">
+          <svg-icon icon-class="password"></svg-icon>
+        </span>
+        <el-input
+          name="password"
+          :type="pwdType"
+          v-model="loginForm.password"
+          autoComplete="on"
+          placeholder="password"
+        ></el-input>
+        <span class="show-pwd" @click="showPwd"
+          ><svg-icon icon-class="eye"
+        /></span>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" style="width: 100%" :loading="loading">
+          Sign in
+        </el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
-<script setup></script>
+<script>
+export default {
+  name: 'login',
+  data() {
+    const validatePass = (rule, value, callback) => {
+      if (value.length < 3) {
+        callback(new Error('密码不能小于3位'))
+      } else {
+        callback()
+      }
+    }
+    return {
+      loginForm: {
+        username: 'admin',
+        password: 'admin'
+      },
+      loginRules: {
+        username: [{ required: true, trigger: 'blur' }],
+        password: [{ required: true, trigger: 'blur', validator: validatePass }]
+      },
+      loading: false,
+      pwdType: 'password',
+      showDialog: false
+    }
+  },
+  methods: {
+    showPwd() {
+      if (this.pwdType === 'password') {
+        this.pwdType = ''
+      } else {
+        this.pwdType = 'password'
+      }
+    },
+    handleLogin() {}
+  }
+}
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+$bg: #2d3a4b;
+$dark_gray: #889aa4;
+$light_gray: #eee;
+$cursor: #fff;
+
+.login-container {
+  min-height: 100%;
+  width: 100%;
+  background-color: $bg;
+  overflow: hidden;
+
+  .login-form {
+    position: relative;
+    width: 520px;
+    max-width: 100%;
+    padding: 160px 35px 0;
+    margin: 0 auto;
+    overflow: hidden;
+
+    ::v-deep .el-form-item {
+      border: 1px solid rgba(255, 255, 255, 0.1);
+      background: rgba(0, 0, 0, 0.1);
+      border-radius: 5px;
+      color: #454545;
+    }
+
+    ::v-deep .el-input {
+      display: inline-block;
+      height: 47px;
+      width: 85%;
+
+      input {
+        background: transparent;
+        border: 0px;
+        -webkit-appearance: none;
+        border-radius: 0px;
+        padding: 12px 5px 12px 15px;
+        color: $light_gray;
+        height: 47px;
+        caret-color: $cursor;
+      }
+    }
+  }
+
+  .svg-container {
+    padding: 6px 5px 6px 15px;
+    color: $dark_gray;
+    vertical-align: middle;
+    display: inline-block;
+  }
+
+  .title-container {
+    position: relative;
+
+    .title {
+      font-size: 26px;
+      color: $light_gray;
+      margin: 0px auto 40px auto;
+      text-align: center;
+      font-weight: bold;
+    }
+  }
+
+  .show-pwd {
+    position: absolute;
+    right: 10px;
+    top: 7px;
+    font-size: 16px;
+    color: $dark_gray;
+    cursor: pointer;
+    user-select: none;
+  }
+}
+</style>
