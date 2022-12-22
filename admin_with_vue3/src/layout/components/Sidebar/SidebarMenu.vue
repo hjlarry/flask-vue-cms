bac462c0020ae2074e9e72e40e72d47c3e5c5938
hejl <hjlarry@163.com>
2022-12-22 16:01:46 +0800

    add the sidebar menu

---
+++ admin_with_vue3/src/layout/components/Sidebar/SidebarMenu.vue
@@ -0,0 +1,23 @@
+<template>
+<el-menu :unique-opened='true' default-active='2' background-color='#545c64' text-color='#fff' active-text-color='#ffd04b'>
+  <el-submenu index='1'>
+    <template #title>
+      <i class="el-icon-location"></i>
+      <span>导航一</span>
+    </template>
+    <el-menu-item index="1-1">选项1</el-menu-item>
+    <el-menu-item index="1-2">选项2</el-menu-item>
+    <el-menu-item index="1-3">选项3</el-menu-item>
+  </el-submenu>
+  <el-menu-item index="4"><i class="el-icon-setting"></i>
+    <span>导航四</span></el-menu-item>
+</el-menu>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
--- admin_with_vue3/src/layout/components/Sidebar/index.vue
+++ admin_with_vue3/src/layout/components/Sidebar/index.vue
@@ -1,3 +1,12 @@
 <template>
-  <div>Side Bar</div>
+  <div class=''>
+    <H1>Side Bar</H1>
+    <el-scrollbar>
+      <sidebar-menu></sidebar-menu>
+    </el-scrollbar>
+  </div>
 </template>
+
+<script setup>
+import SidebarMenu from './SidebarMenu.vue'
+</script>
