c4ffe3800f4159fe95a4df40d22c48e4320879b0
hejl <hjlarry@163.com>
2023-01-05 10:30:13 +0800

    can get the data from excel

--- admin_with_vue3/src/utils/request.js
+++ admin_with_vue3/src/utils/request.js
@@ -35,7 +35,7 @@ service.interceptors.response.use(
       })
 
       // 50008:非法的token; 50012:其他客户端登录了;  50014:Token 过期了;
-      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
+      if (res.code === 401 || res.code === 50012 || res.code === 50014) {
         ElMessageBox.confirm(
           '你已被登出，可以取消继续留在该页面，或者重新登录',
           '确定登出',
@@ -45,6 +45,7 @@ service.interceptors.response.use(
             type: 'warning'
           }
         ).then(() => {
+          store.dispatch('user/logout')
           // store.dispatch('FedLogOut').then(() => {
           //   location.reload() // 为了重新实例化vue-router对象 避免bug
           // })
--- admin_with_vue3/src/views/import/index.vue
+++ admin_with_vue3/src/views/import/index.vue
@@ -6,6 +6,7 @@
     :limit="1"
     :show-file-list="false"
     :on-change="handleUpload"
+    v-loading="loading"
   >
     <SvgIcon icon="cloud-upload"></SvgIcon>
     <div class="el-upload__text">
@@ -17,13 +18,41 @@
 <script setup>
 import SvgIcon from '@/components/SvgIcon'
 import { ElMessage } from 'element-plus'
+import { read, utils } from 'xlsx'
+import { ref } from 'vue'
+
+const loading = ref(false)
+
+function isExcel(file) {
+  return /\.(xlsx|xls|csv)$/.test(file.name)
+}
 
 function handleUpload(file) {
-  console.log(file)
-  if (!(file.name.endsWith('.xlsx') || file.name.endsWith('.xls'))) {
+  if (!isExcel(file)) {
     ElMessage.error('文件类型错误')
     return false
   }
+  readData(file.raw)
+}
+
+function readData(file) {
+  loading.value = true
+  // 异步读取数据
+  new Promise((resolve) => {
+    const reader = new FileReader()
+    reader.onload = (e) => {
+      const data = e.target.result
+      const workbook = read(data, { type: 'binary' })
+      const sheetNames = workbook.SheetNames
+      const worksheet = workbook.Sheets[sheetNames[0]]
+      const json = utils.sheet_to_json(worksheet)
+      resolve(json)
+    }
+    reader.readAsBinaryString(file)
+  }).then((res) => {
+    console.log(res)
+    loading.value = false
+  })
 }
 </script>
 
