112d594e4f8a7d415d51886222716f0a3334a1bb
hejl <hjlarry@163.com>
2023-01-13 13:57:17 +0800

    add 401 and 404

--- admin_with_vue3/src/views/error-page/401.vue
+++ admin_with_vue3/src/views/error-page/401.vue
@@ -1,9 +1,64 @@
 <template>
-  <div class="">401</div>
+  <div class="errPage-container">
+    <el-row>
+      <el-col :span="12">
+        <h1 class="text-jumbo text-ginormous">Oops!</h1>
+        <h2>你没有权限去该页面</h2>
+      </el-col>
+      <el-col :span="12">
+        <img
+          :src="errGif"
+          width="313"
+          height="428"
+          alt="Girl has dropped her ice cream."
+        />
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script setup>
-import {} from 'vue'
+import gif401 from '@/styles/401_images/401.gif'
+import { ref } from 'vue'
+const errGif = ref(gif401 + '?' + +new Date())
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.errPage-container {
+  width: 800px;
+  max-width: 100%;
+  margin: 100px auto;
+  .pan-back-btn {
+    background: #008489;
+    color: #fff;
+    border: none !important;
+  }
+  .pan-gif {
+    margin: 0 auto;
+    display: block;
+  }
+  .pan-img {
+    display: block;
+    margin: 0 auto;
+    width: 100%;
+  }
+  .text-jumbo {
+    font-size: 60px;
+    font-weight: 700;
+    color: #484848;
+  }
+  .list-unstyled {
+    font-size: 14px;
+    li {
+      padding-bottom: 5px;
+    }
+    a {
+      color: #008489;
+      text-decoration: none;
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+}
+</style>
