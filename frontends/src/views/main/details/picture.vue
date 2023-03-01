b919afebc8038c524689559c4e5026fb9fd07e0a
hejl <hjlarry@163.com>
2023-03-01 14:41:27 +0800

    add big picture mobile display

--- frontends/src/views/main/details/picture.vue
+++ frontends/src/views/main/details/picture.vue
@@ -1,8 +1,19 @@
 <template>
   <div
-    class="fixed top-0 left-0 z-50 w-screen h-screen bg-white backdrop-blur-4xl xl:bg-transparent xl:p-2"
+    class="fixed top-0 left-0 z-50 w-screen h-screen overflow-y-auto bg-white backdrop-blur-4xl xl:bg-transparent xl:p-2"
   >
+    <m-navbar v-if="isMobileDevice" sticky
+      >{{ data.title }}
+      <template #right>
+        <m-svg-icon
+          name="share"
+          class="w-3 h-3"
+          fillClass="fill-zinc-900 dark:fill-zinc-200"
+        ></m-svg-icon>
+      </template>
+    </m-navbar>
     <m-svg-icon
+      v-else
       name="close"
       class="absolute right-2 top-2 w-3 h-3 p-0.5 cursor-pointer rounded-sm hover:bg-zinc-100"
       fillClass="fill-zinc-400"
@@ -14,9 +25,9 @@
         :src="imgURL"
       />
       <div
-        class="xl:w-2/5 xl:h-full xl:rounded-tr-lg xl:rounded-br-lg xl:p-3 xl:bg-white xl:dark:bg-zinc-900"
+        class="bg-white dark:bg-zinc-900 xl:w-2/5 xl:h-full xl:rounded-tr-lg xl:rounded-br-lg xl:p-3"
       >
-        <div class="flex justify-between">
+        <div v-if="!isMobileDevice" class="flex justify-between">
           <m-svg-icon
             name="share"
             class="h-4 w-4 p-1 hover:bg-zinc-200 duration-500 rounded cursor-pointer dark:hover:bg-zinc-700"
@@ -30,12 +41,12 @@
         </div>
 
         <p
-          class="text-base text-zinc-900 dark:text-zinc-200 ml-1 mt-2 font-bold xl:text-xl"
+          class="text-base text-zinc-900 dark:text-zinc-200 ml-1 pt-2 font-bold xl:text-xl"
         >
           {{ data.title }}
         </p>
 
-        <div class="flex items-center mt-5 px-1">
+        <div class="flex items-center py-3 px-1">
           <img class="w-3 h-3 rounded-full" :src="data.avatar" />
           <span class="text-base text-zinc-900 dark:text-zinc-200 ml-1">{{
             data.author
@@ -48,6 +59,8 @@
 
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
+
+import { isMobileDevice } from '@/utils/flexiable'
 
 const props = defineProps<{ data: any }>()
 
