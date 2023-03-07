c3728045267326eecec63c4b1592fb1ac4473a47
hejl <hjlarry@163.com>
2023-03-07 10:41:09 +0800

    add cropperjs to modify avatar

--- frontends/src/views/profile/change-avatar.vue
+++ frontends/src/views/profile/change-avatar.vue
@@ -5,12 +5,23 @@
       class="w-3 h-3 ml-auto p-0.5 m-1"
       @click="onCloseClick"
     />
-    <img :src="blob" />
-    <m-button class="mt-4 mb-1 w-[80%] xl:w-1/2">确定</m-button>
+
+    <img :src="blob" ref="imgTarget" />
+
+    <m-button
+      class="mt-4 mb-1 w-[80%] xl:w-1/2"
+      :isActiveAnim="false"
+      @click="onConfirmClick"
+      >确定</m-button
+    >
   </div>
 </template>
 
 <script setup lang="ts">
+import { userStore } from '@/stores/user'
+import Cropper from 'cropperjs'
+import { ref, onMounted } from 'vue'
+
 defineProps<{
   blob: string
 }>()
@@ -18,6 +29,60 @@ const emits = defineEmits(['close'])
 
 const onCloseClick = () => {
   emits('close')
+}
+const imgTarget = ref<HTMLImageElement | null>(null)
+
+const pc_template = `
+<cropper-canvas background style="min-height:300px;min-width:300px">
+  <cropper-image></cropper-image>
+  <cropper-shade hidden></cropper-shade>
+  <cropper-handle action="select" plain></cropper-handle>
+  <cropper-selection initial-coverage="0.5" movable resizable zoomable>
+    <cropper-grid role="grid" bordered covered></cropper-grid>
+    <cropper-crosshair theme-color="rgba(238, 238, 238, 0.5)" centered></cropper-crosshair>
+    <cropper-handle action="move" theme-color="rgba(255, 255, 255, 0.35)"></cropper-handle>
+    <cropper-handle action="n-resize"></cropper-handle>
+    <cropper-handle action="e-resize"></cropper-handle>
+    <cropper-handle action="s-resize"></cropper-handle>
+    <cropper-handle action="w-resize"></cropper-handle>
+    <cropper-handle action="ne-resize"></cropper-handle>
+    <cropper-handle action="nw-resize"></cropper-handle>
+    <cropper-handle action="se-resize"></cropper-handle>
+    <cropper-handle action="sw-resize"></cropper-handle>
+  </cropper-selection>
+</cropper-canvas>
+`
+const mobile_template = `
+<cropper-canvas background style="min-height:300px;min-width:300px">
+  <cropper-image></cropper-image>
+  <cropper-shade hidden></cropper-shade>
+  <cropper-handle action="select" plain></cropper-handle>
+  <cropper-selection initial-coverage="0.5" movable resizable zoomable>
+    <cropper-handle action="move" theme-color="rgba(255, 255, 255, 0.35)"></cropper-handle>
+  </cropper-selection>
+</cropper-canvas>
+`
+
+const uStore = userStore()
+
+let cropper: Cropper
+onMounted(() => {
+  cropper = new Cropper(imgTarget.value!, {
+    template: mobile_template
+  })
+})
+
+const onConfirmClick = () => {
+  cropper
+    .getCropperSelection()
+    ?.$toCanvas()
+    .then((canvas) => {
+      uStore.setUserInfo({
+        ...uStore.userInfo,
+        avatar: canvas.toDataURL('image/jpeg', 0.5)
+      })
+    })
+  location.reload()
 }
 </script>
 
